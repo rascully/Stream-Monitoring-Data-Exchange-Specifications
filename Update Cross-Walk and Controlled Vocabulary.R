2477c388ca479e9cce560a412bc5bfaaaf289299
Rebecca <rscully@usgs.gov>
2020-06-03 08:52:14 -0700

    add cross-walk and controlled vocab

---
+++ Update Cross-Walk and Controlled Vocabulary.R
@@ -0,0 +1,9 @@
+#Need to design a workflow to link the master metadata file to the individual files or figure out a way to update each file. 
+install.packages('tidyverse')
+library(tidyverse)
+
+#load the metadata file from the GitRepository library(httr)
+
+library(httr)
+library(xlsx)
+

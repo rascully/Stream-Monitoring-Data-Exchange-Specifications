d584a6fe015fd0a32b7e9610fe2b6f117084e039
Scully <RSCULLY@USGS.GOV>
2021-01-13 22:00:34 -0800

    Update tables

--- R/Create_Data_Tables.R
+++ R/Create_Data_Tables.R
@@ -6,12 +6,14 @@ library(tidyverse)
 metadata <- readxl::read_excel("Metadata.xlsx", sheet = 3)
 
 #Create the data exchange specifications tables 
-DES_tables <- c("RecordLevel", "Location","Event")
+DES_tables <- c("RecordLevel", "Location","Event", "MeasurementOrFact")
 for (i in 1:length(DES_tables)){ 
      x<- metadata %>% 
             select(CategoryID, FieldID, Category, InDES, Field,Description,Examples, DataType ) %>% 
             filter(Category== DES_tables[i], InDES=="x") %>% 
             select(-InDES)
+      
+      print(DES_tables[i]) 
       file_name <- paste0(DES_tables[i], "_table") # create a file name 
       write.csv(x, file=paste0("Tables/",Sys.Date(),"_",file_name,".csv" ))
   }

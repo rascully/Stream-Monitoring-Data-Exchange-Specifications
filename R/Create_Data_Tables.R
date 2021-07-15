428c5ceedc1201920786be653dd97a9efbc730a3
Scully <RSCULLY@USGS.GOV>
2021-07-15 11:39:47 -0700

    update tables

--- R/Create_Data_Tables.R
+++ R/Create_Data_Tables.R
@@ -34,7 +34,7 @@ crosswalk<- metadata %>%
         filter(SubsetOfMetrics=="x"| InDES=="x"  ) %>% 
         select(-SubsetOfMetrics, -InDES) 
         
-crosswalk[str_detect(crosswalk$measurementTerm, c("verbatimLongitude","verbatimLatitude")),]
+crosswalk[str_detect(crosswalk$measurementTerm, c("verbatimLongitude","verbatimLatitude", "Protocol")),]
   
 names(crosswalk) <- str_remove_all(names(crosswalk), "CW")
 write.csv(crosswalk, file=paste0("Tables/Crosswalk.csv" ), row.names=F)
